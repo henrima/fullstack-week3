3ea3e164b3f0935f4c38ba5bd6e52db3fe93e555
Henri Malkki <henri.malkki@perfektio.fi>
2018-02-12 18:43:50 +0200

    3.15

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -6,15 +7,29 @@ const url = process.env.MLAB_URI
 
 mongoose.connect(url)
 
-const Person = mongoose.model('Person', {
-  name: {
-      type: String,
-      required: [true, 'Henkilöllä pitää olla nimi.']
-  },
-  number: {
-      type: String,
-      required: [true, 'Henkilöllä pitää olla numero.']
+const personSchema = new Schema({  
+    name: {
+    type: String,
+    required: [true, 'Henkilöllä pitää olla nimi.']
+},
+    number: {
+        type: String,
+        required: [true, 'Henkilöllä pitää olla numero.']
     }    
 })
+
+const Person = mongoose.model('Person', personSchema)
+
+// personSchema.methods.format = function(person, cb) {
+//     const filteredPerson = {
+//         "name": person.name,
+//         "number": person.number,
+//         "id": person._id
+//     }
+//     format.addCallback(function () {
+//         console.log('Person filtered.')
+//     }, cb)
+//   }
+
 
 module.exports = Person
